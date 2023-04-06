a3b229b7e9b6730d338525a9d0c70ad09147d052
Alves <alves@babbels.nl>
2023-04-06 19:51:58 +0200

    wip: change captioning and make full pipeline

---
+++ prompts/query_prompts.py
@@ -0,0 +1,7 @@
+# Make sure there prompt is in the correct order as the images
+query_prompts_for_diffusion = [
+    "turn the car color blue",
+    "turn the car color green",
+    "make the dog less fluffy",
+    "make the dog more fluffy",
+]
--- query_prompts.py
+++
@@ -1,5 +0,0 @@
-# Make sure there prompt is in the correct order as the images
-query_prompts_for_diffusion = [
-    "make it into one that is black colored",
-    "make it into one that is orange colored",
-]
