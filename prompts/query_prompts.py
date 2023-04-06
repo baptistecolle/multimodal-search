fa4a6be092861990ad577d31d22ec96bdf50a5bd
Alves <alves@babbels.nl>
2023-04-06 20:55:56 +0200

    working pipeline

--- prompts/query_prompts.py
+++ prompts/query_prompts.py
@@ -1,7 +1,7 @@
 # Make sure there prompt is in the correct order as the images
 query_prompts_for_diffusion = [
-    "turn the car color blue",
-    "turn the car color green",
-    "make the dog less fluffy",
-    "make the dog more fluffy",
+    "turn the ferrari from red to blue",
+    "turn the lamborghini from orange to green",
+    "make the foxy dog less fluffy",
+    "make the pug more fluffy",
 ]
