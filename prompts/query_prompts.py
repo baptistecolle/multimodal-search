ac991f8faad5b4cac810fa5112d50d3c8251a54c
Radek Kargul <radoslaw.kargul@gmail.com>
2023-04-07 14:29:50 +0200

    Add query images and prompts (24 total)

--- prompts/query_prompts.py
+++ prompts/query_prompts.py
@@ -5,3 +5,31 @@ query_prompts_for_diffusion = [
     "make the foxy dog less fluffy",
     "make the pug more fluffy",
 ]
+
+#
+query_prompts_for_input_images = [
+    "make this cat lick itself",
+    "put-this–elephant–in–a–zoo",
+    "make this car new and clean",
+    "turn the race car red color",
+    "turn the t-shirt rainbow colors",
+    "make these converse boots low",
+    "put this iPhone on a table",
+    "close  the laptop lid",
+    "take this katana out of the sheath",
+    "make this person hold two guns",
+    "show Eiffel Tower at night",
+    "put camels next to the pyramid",
+    "move this flower to a desert",
+    "add tepals to the cacti",
+    "solve this Rubik's cube",
+    "make this Rubik's cube 4 by 4 size",
+    "put the hot air balloon on sand",
+    "show only one white and red hot air balloon",
+    "make this jack Daniels whiskey instead",
+    "make this beer glass half empty",
+    "make this ferrari yellow",
+    "make this lamborghini a faster model",
+    "remove the fur from this dog",
+    "make this pug darker fur and smile less"
+]
